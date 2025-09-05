f0aea4780123526848c2c5f3cf3832d2c1c524e1
Alessandro Valentini <alessandro.valentini.90@gmail.com>
2025-09-05 17:17:46 +0200

    add arduino binaries to deb package

--- Arduino/upload2board.sh
+++ Arduino/upload2board.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 set -e
 
-BUILD_DIR=$1
-DEFAULT_FQBN=$2
+# Those defaults are needed for the built package
+BUILD_DIR=${1:-/usr/share/spear/firmware/}
+DEFAULT_FQBN=${2:-arduino:avr:nano:cpu=atmega328old}
 
 AC_RESULT=$(arduino-cli board list | grep "Serial Port (USB)" | sed "s/Serial Port (USB)//g")
 
-PORT=$(echo ${AC_RESULT} | awk '{print $1}')
-FQBN=$(echo ${AC_RESULT} | awk '{print $3}')
+PORT=$(echo "${AC_RESULT}" | awk '{print $1}')
+FQBN=$(echo "${AC_RESULT}" | awk '{print $3}')
 
-if [[ "${FQBN}" == "Unknown" ]]; then
+if [[ "${FQBN}" == "Unknown" || -z "${FQBN}" ]]; then
   echo "Unknown Port detected, using default = ${DEFAULT_FQBN}"
   FQBN=${DEFAULT_FQBN}
 fi
 
 echo "Uploading code to board ${FQBN} on port ${PORT}"
-arduino-cli upload -v -p "${PORT}" -b "${FQBN}" --input-dir ${BUILD_DIR}
+arduino-cli upload -v -p "${PORT}" -b "${FQBN}" --input-dir "${BUILD_DIR}"
 echo "Upload successful!"
